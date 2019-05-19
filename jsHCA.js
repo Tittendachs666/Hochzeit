94980c14c84adffdb2be2dab6c3cd039f14208b1
Tittendachs666 <m.ruecker46@gmail.com>
2019-05-19 20:40:58 +0200

    timer function

--- jsHCA.js
+++ jsHCA.js
@@ -1,18 +1,32 @@
-var C = document.getElementById("Cassian");
+function Timer() {
 
-C.onclick = function CaBereich () {
- alert (46);
+var now = new Date().getTime();
+
+var distance = timerdate - now;
+
+var days = Math.floor((distance /(1000 * 60 *60 * 24)) / (1000 * 60 * 60 * 24));
+
+var hours = Math.floor((distance /(1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+document.getElementById("timer").innerHTML = days + " Tage  " + hours + " Stunden  " + minutes + "m " + seconds + "s";
+
+if (distance < 0) {
+ clearInterval(x);
+ document.getElementById("timer").innerHTML = "Jo Hochzeit steht an! Partytime!"
+ }
+
 }
 
 
-alert (46);
 
 
+var timerdate = new Date("Aug 24, 2019 13:00:00").getTime();
 
-
-
-
-
+var x = setInterval(Timer(),1000); 
 
 
 
